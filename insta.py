d2c3eb53155280727d7ada551bc5d38fe7059464
Lal Krishna <134937814+STARK-404@users.noreply.github.com>
2025-02-06 20:46:34 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -1,4 +1,4 @@
-import zipfile
+import pyzipper
 import os
 from rich.panel import Panel
 from rich.console import Console
@@ -11,32 +11,26 @@ ___logo___ = (Panel.fit(r'''
                                     
 ''',style='bold magenta',title='[bold blue](Instagram Brute Force)'))
 
-
 def extract_zip(zip_path):
-    extract_to = os.path.dirname(zip_path)
+    extract_to = os.path.dirname(zip_path)  
     while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
         print(___logo___)
         print("[bold white][[bold green]![bold white]] Enter the password to extract the ZIP file]")
         password = input("\033[35m└─>").encode('utf-8')
-        
         try:
-            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            with pyzipper.AESZipFile(zip_path, 'r') as zip_ref:
                 zip_ref.extractall(path=extract_to, pwd=password)
                 print("[bold white][[bold green]![bold white]] Extraction successful!")
-                break
+                break  
         except RuntimeError:
-            print("[bold white][[bold red]![bold white]] Incorrect password. Try again.")
-            os.system('xdg-open mailto:gamerunknown509@gmail.com?subject=I want Insta Password&body=Hello, I am using your tool and I need the password of the zip file. Please provide me the password.')
-        except zipfile.BadZipFile:
-            print("Invalid ZIP file.")
+             print("[bold white][[bold red]![bold white]] Incorrect password. Try again.")
+             os.system('xdg-open mailto:gamerunknown509@gmail.com?subject=I want Insta Password&body=Hello, I am using your tool and I need the password of the zip file. Please provide me the password.')
+        except pyzipper.zipfile.BadZipFile:
+            print("[bold red] Invalid ZIP file.")
             return
-    
     os.remove(zip_path)
-    
+   
 
 if __name__ == "__main__":
     zip_file_path = "Run.zip" 
-
-    
     extract_zip(zip_file_path)
