ceea34114d2b14719feed64f6fcbd23fdfde3cab
STARK-404 <134937814+STARK-404@users.noreply.github.com>
2026-08-29 14:56:06 +0530

    Fix indentation errors in insta.py and clean up exception handling

--- insta.py
+++ insta.py
@@ -1,4 +1,3 @@
-
 import time
 import pyzipper
 import os
@@ -17,48 +16,50 @@ ___logo___ = Panel.fit(
  ___       __ ___             _   _ 
   |  |\ | (_   |  /\ __ |\/| |_) |_ 
  _|_ | \| __)  | /--\   |  | |_) |  
-                                    
+                                     
 ''',
     style='bold magenta',
     title='[bold blue](Instagram Brute Force)'
 )
 
+
 def extract_zip(zip_path):
     extract_to = os.path.dirname(zip_path)
     while True:
         os.system('cls' if os.name=='nt' else 'clear')
-        console.print(___logo___) 
+        console.print(___logo___)
         console.print("[bold white][[bold green]![bold white]] Enter the password to extract the ZIP file]")
-        password = getpass.getpass("\033[35m└─>").encode('utf-8') 
+        password = getpass.getpass("\033[35m└─>").encode('utf-8')
         try:
             with pyzipper.AESZipFile(zip_path, 'r') as zip_ref:
                 zip_ref.extractall(path=extract_to, pwd=password)
                 console.print("[bold white][[bold green]![bold white]] Extraction successful!")
-                os.system("python Run.py") 
-                break 
+                os.system("python Run.py")
+                break
         except RuntimeError:
             console.print("[bold white][[bold red]![bold white]] Incorrect password. Try again.")
-           console.print("[!]  If you want to use Instahack buy the password from next page when you click enter! \n Press enter \n You Have Been Redirected To Payment Page!! \n any enquires contact me @gamerunknown509@gmail.com ")
-            
-
-            os.system('xdg-open https://buymeacoffee.com/mrstarkin/e/471721')
+            console.print("[!] If you want to use Instahack buy the password from next page when you click enter!\nPress enter to be redirected to the payment page. For any enquiries contact the author.")
+            try:
+                # Try to open payment page on supported systems
+                os.system('xdg-open https://buymeacoffee.com/mrstarkin/e/471721')
+            except Exception:
+                pass
             time.sleep(6)
         except pyzipper.zipfile.BadZipFile:
             console.print("[bold red] Invalid ZIP file.")
             return
         except KeyboardInterrupt:
             os.system('cls' if os.name=='nt' else 'clear')
-            console.print(__logo__)
+            console.print(___logo___)
             console.print(Panel.fit("[bold red] Bye See You Soon "))
+            return
+
+    try:
+        os.remove(zip_path)
+    except OSError:
+        pass
 
 
-            
-    os.remove(zip_path) 
 if __name__ == "__main__":
-    zip_file_path = "Run.zip" 
-    extract_zip(zip_file_path)  
-
-
-
-
-
+    zip_file_path = "Run.zip"
+    extract_zip(zip_file_path)
